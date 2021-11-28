c9f10c99b43f5e5461d1ae93e19e03b3b116f8bf
Aisyah Zahrotul Hidayah <43850912+hydaai@users.noreply.github.com>
2021-11-28 21:43:52 +0700

    Update D. Pricing and Ratings.sql

--- Case Study #2 - Pizza Runner/Scripts/D. Pricing and Ratings.sql
+++ Case Study #2 - Pizza Runner/Scripts/D. Pricing and Ratings.sql
@@ -33,7 +33,6 @@ from
 		)a
 	)b;
 
-select 'create schema' as title
 -- 3. The Pizza Runner team now wants to add an additional ratings system that allows customers to rate their runner, how would you design an additional table for this new dataset - generate a schema for this new table and insert your own data for ratings for each successful customer order between 1 to 5.
 DROP TABLE IF EXISTS #rating_orders;
 CREATE TABLE #rating_orders (
