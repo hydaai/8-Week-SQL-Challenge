4ec4cd96e6ad19ee01e08cfbd207ddd895da6d5f
Aisyah Zahrotul Hidayah <43850912+hydaai@users.noreply.github.com>
2021-11-28 21:16:29 +0700

    Update D. Pricing and Ratings.sql

--- Case Study #2 - Pizza Runner/Scripts/D. Pricing and Ratings.sql
+++ Case Study #2 - Pizza Runner/Scripts/D. Pricing and Ratings.sql
@@ -6,7 +6,6 @@ from #customer_orders c
 left join #runner_orders r on r.order_id = c.order_id
 where distance != 0;
 
-select 'profit extras' as title
 -- 2. What if there was an additional $1 charge for any pizza extras? Add cheese is $1 extra
 select 
 	sum(money) profit
@@ -38,30 +37,29 @@ select 'create schema' as title
 -- 3. The Pizza Runner team now wants to add an additional ratings system that allows customers to rate their runner, how would you design an additional table for this new dataset - generate a schema for this new table and insert your own data for ratings for each successful customer order between 1 to 5.
 DROP TABLE IF EXISTS #rating_orders;
 CREATE TABLE #rating_orders (
-  order_id INTEGER identity(1,1),
+  order_id INTEGER,
   rating INTEGER,
   review VARCHAR(max)
 );
 
 INSERT INTO #rating_orders
-  (rating, review)
+  (order_id, rating, review)
 VALUES
-  (1, 'Really bad service'),
-  (2, null),
-  (4, 'Good service'),
-  (2, 'Pizza arrived cold and took long'),
-  (3, null),
-  (3, null),
-  (5, 'It was great, good service and fast'),
-  (4, 'Not bad');
+  (1, 1, 'Really bad service'),
+  (2, 2, null),
+  (3, 4, 'Good service'),
+  (4, 2, 'Pizza arrived cold and took long'),
+  (5, 3, null),
+  (7, 3, null),
+  (8, 5, 'It was great, good service and fast'),
+  (10, 4, 'Not bad');
 select * from #rating_orders;
 
-select 'create combine table' as title
 -- 4. Using your newly generated table - can you join all of the information together to form a table which has the following information for successful deliveries? (customer_id, order_id, runner_id, rating, order_time, pickup_time, Time between order and pickup, Delivery duration, Average speed, Total number of pizzas)
 DROP TABLE IF EXISTS #generated_table;
 select 
-	c.customer_id, r.runner_id, ro.rating, 
-	c.order_time, r.pickup_time,
+	c.customer_id, c.order_id, r.runner_id,
+	ro.rating, c.order_time, r.pickup_time,
 	DATEPART(MINUTE, r.pickup_time - c.order_time) time_between_order_and_pickup,
 	r.duration delivery_duration,
 	round(r.distance/r.duration * 60, 2) average_speed,
@@ -71,10 +69,10 @@ from #rating_orders ro
 left join #customer_orders c on c.order_id = ro.order_id
 left join #runner_orders r on ro.order_id = r.order_id
 where r.distance != 0
-group by c.customer_id, r.runner_id, ro.rating, c.order_time, r.pickup_time, r.distance, r.duration;
-select * from #generated_table;
+group by c.customer_id, c.order_id, r.runner_id, ro.rating, c.order_time, r.pickup_time, r.distance, r.duration;
+select * from #generated_table
+order by order_id;
 
-select 'money left' as title
 -- 5. If a Meat Lovers pizza was $12 and Vegetarian $10 fixed prices with no cost for extras and each runner is paid $0.30 per kilometre traveled - how much money does Pizza Runner have left over after these deliveries?
 select 
 	sum(money) money_left
